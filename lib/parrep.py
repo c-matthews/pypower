2383795114b00e775841ccb7d43fabd8139ebc40
c_matthews <c.matthews@uchicago.edu>
2016-08-03 17:16:32 -0500

    Removed Fleming-Viot process

--- lib/parrep.py
+++ lib/parrep.py
@@ -12,7 +12,9 @@ class ParRep:
         self.ig = integrator
         self.output = output
           
-        self.maxtime = ini.getfloat("parrep","maxtime")  
+        self.stoptime = ini.getfloat("parrep","stop_time", 10000.0)  
+        self.stoplines = ini.getint("parrep","stop_lines", self.model.nline)  
+        self.stopload = ini.getfloat("parrep","stop_load", 0.0)  
         self.walkers = ini.getint("parrep","walkers",1) 
         self.t_decor = ini.getfloat("parrep","t_decor")  
         self.t_dephase = ini.getfloat("parrep","t_dephase")  
@@ -40,15 +42,17 @@ class ParRep:
         
         return GG,LSLS,TT,enum 
     
-    def sync_state(self, f,a,m,gamma):
+    def sync_state(self, f,a,m,gamma, time, ls):
         
         
         f = self.comm.bcast( f , root=0 )
         a = self.comm.bcast( a , root=0 )
         m = self.comm.bcast( m , root=0 )
         gamma = self.comm.bcast( gamma , root=0 )
+        time = self.comm.bcast( time , root=0 )
+        ls = self.comm.bcast( ls , root=0 )
         
-        return f,a,m,gamma
+        return f,a,m,gamma,time,ls
         
         
         
@@ -70,11 +74,14 @@ class ParRep:
             enum = 0
             
             time = 0.0
+            ls = 1.0
             
             if (myid==0):
-                print " -- Beginning run %d." % (repnum)
+                print ""
+                print " -- Beginning run %d." % (repnum+1)
+                print ""
             
-            while (time < self.maxtime):
+            while (  self.keepgoing( time , gamma, ls  )  ):
                 
                 ####
                 # DECOR STAGE
@@ -82,9 +89,9 @@ class ParRep:
                 
                 if (myid==0):
                     jj = 0
-                    print "DECOR step, time: %f    events: %d." % (time, enum)
+                    print "DECOR step, time: %f  ls:%f    events: %d." % (time, ls, enum)
                     
-                    while (jj < self.s_decor):
+                    while (jj < self.s_decor) and ( self.keepgoing( time , gamma, ls  ) ):
                         #f,a,m,F,A,M,E,L,jj,g,nls = self.ig.adv( f, a , m , self.s_decor , gamma )
                         #f,a,m,jj,gamma,nls = self.ig.adv( f, a , m , self.s_decor , gamma )[0,1,2,8,9,10]
                         f,a,m,_,_,_,_,_,jj,g,nls = self.ig.adv( f, a , m , self.s_decor , gamma ) 
@@ -92,79 +99,62 @@ class ParRep:
                         time += jj * self.ig.dt
                         gg = gamma
                         gamma = g
+                        ls = nls
                         
                         if (self.diff_g(g,gg) ):
                             G,LS,T,enum = self.add_event(G,LS,T,enum, gamma, nls, time )
                             jj=0
-                            print " - event, time: %f    events: %d." % (time, enum)
+                            print " - event, time: %f   ls: %f     events: %d." % (time, ls , enum)
                             
-                        if (time>= self.maxtime):
-                            break
+                
+                f,a,m,gamma,time,ls = self.sync_state( f,a,m,gamma,time, ls )
+                
+                if (self.keepgoing( time , gamma, ls  ) == False ):
+                    break
                 
                 ####
                 # DEPHASE STAGE
-                ####
+                #### 
                 
-                
-                if (myid==0) and time<self.maxtime:
-                    jj = 0  
-                    F = np.tile(f, (self.walkers,1) ).T
-                    A = np.tile(a, (self.walkers,1) ).T
-                    M = np.tile(m, (self.walkers,1) ).T
-                    print "DEPHASE step, time: %f    events: %d." % (time, enum)
+                MyTasks = np.arange(myid, self.walkers, self.comm.Get_size())
+                ntasks = len(MyTasks)
+                F = np.tile(f, (ntasks,1) ).T
+                A = np.tile(a, (ntasks,1) ).T
+                M = np.tile(m, (ntasks,1) ).T
+                if (myid==0):
+                    print "DEPHASE step, time: %f  ls:%f    events: %d." % (time, ls, enum)
                     
+                for ii in np.arange( ntasks ) :
+                    f = F[:,ii]
+                    a = A[:,ii]
+                    m = M[:,ii] 
+                    tt = 0
                     
-                    for jj in np.arange( self.s_dephase): 
-                    
-                        for ii in np.arange( self.walkers ):
-                            f = F[:,ii]
-                            a = A[:,ii]
-                            m = M[:,ii] 
-                            tt = 0
-                            
-                            while (tt==0):
-                                f,a,m,_,_,_,_,_,tt,g,_ = self.ig.adv( f, a , m , 1 , gamma ) 
+                    while (tt==0):
+                        f,a,m,_,_,_,_,_,tt,g,_ = self.ig.adv( f, a , m , self.s_dephase , gamma ) 
                                 
-                                if (self.diff_g(g,gamma)): 
-                                    ri =  self.model.Random.randint( ii+1 ) 
-                                    f = F[:,ri]
-                                    a = A[:,ri]
-                                    m = M[:,ri]
-                                    tt = 0
+                        if (self.diff_g(g,gamma)): 
+                            ri =  self.model.Random.randint( ii+1 ) 
+                            f = F[:,ri]
+                            a = A[:,ri]
+                            m = M[:,ri]
+                            tt = 0
                                     
-                            F[:,ii] = f
-                            A[:,ii] = a
-                            M[:,ii] = m
-                        
+                    F[:,ii] = f
+                    A[:,ii] = a
+                    M[:,ii] = m
+                     
                 
                 ####
                 # PARALLEL STAGE
                 ####
-                
-                if (myid==0):
-                    FF = [F[:, np.arange(jj, self.walkers, self.comm.Get_size())]  for jj in np.arange(self.comm.Get_size() )  ] 
-                    AA = [A[:, np.arange(jj, self.walkers, self.comm.Get_size())]  for jj in np.arange(self.comm.Get_size() )  ] 
-                    MM = [M[:, np.arange(jj, self.walkers, self.comm.Get_size())]  for jj in np.arange(self.comm.Get_size() )  ] 
-                    print "PARALLEL step, time: %f    events: %d." % (time, enum)
-                else:
-                    FF= None
-                    AA = None
-                    MM = None
-                    
-                F = self.comm.scatter( FF  , root = 0 )
-                A = self.comm.scatter( AA  , root = 0 )
-                M = self.comm.scatter( MM  , root = 0 )
-                time = self.comm.bcast( time , root=0 )
-                gamma = self.comm.bcast( gamma , root=0 )
-
-                print [myid , np.shape(F) ]
-
-                ntasks = np.shape(F)[1]
+                if (myid==0): 
+                    print "PARALLEL step, time: %f  ls:%f    events: %d." % (time, ls, enum)
                     
                 event = -1
                 gchk  = -1
 
-                while (time < self.maxtime ):
+                while (self.keepgoing( time , gamma, ls  ) ):
                     
                     for ii in np.arange( ntasks ):
                         
@@ -181,18 +171,18 @@ class ParRep:
                     else:
                         jj = self.comm.bcast( jj , root= gchk )
                         gamma = self.comm.bcast( g , root= gchk )
-                        nls = self.comm.bcast( nls , root= gchk )
+                        ls = self.comm.bcast( nls , root= gchk )
                         f = self.comm.bcast( F[:,ii] , root= gchk )
                         a = self.comm.bcast( A[:,ii] , root= gchk )
                         m = self.comm.bcast( M[:,ii] , root= gchk )
-                        time += self.ig.dt * self.pstep * ( gchk  / self.comm.Get_size() ) + jj * self.ig.dt
+                        time += self.ig.dt * self.pstep * ( float(gchk)  / self.comm.Get_size() ) + jj * self.ig.dt
                         break
                     
                 
                         
                 if (gchk>=0) and (myid==0):
                     G,LS,T,enum = self.add_event(G,LS,T,enum, gamma, nls, time )
-                    print " - event, time: %f    events: %d." % (time, enum)
+                    print " - event, time: %f   ls: %f     events: %d." % (time, ls , enum)
             
             
             if (myid==0):    
@@ -211,3 +201,76 @@ class ParRep:
         else:
             return False
         
+    def keepgoing(self, time, g , ls ):
+        
+        if ( time >= self.stoptime ):
+            return False
+        
+        if ( self.model.nline - np.sum( np.array(g) ) >= self.stoplines ):
+            return False
+        
+        if (ls <= self.stopload ):
+            return False
+        
+        return True
+    
+        
+        
+        
+        
+########
+## Fleming Viot process:
+########
+
+                #if (myid==0) and time<self.maxtime:
+                    #jj = 0  
+                    #F = np.tile(f, (self.walkers,1) ).T
+                    #A = np.tile(a, (self.walkers,1) ).T
+                    #M = np.tile(m, (self.walkers,1) ).T
+                    #print "DEPHASE step, time: %f    events: %d." % (time, enum)
+                    
+                    
+                    #for jj in np.arange( self.s_dephase): 
+                    
+                        #for ii in np.arange( self.walkers ):
+                            #f = F[:,ii]
+                            #a = A[:,ii]
+                            #m = M[:,ii] 
+                            #tt = 0
+                            
+                            #while (tt==0):
+                                #f,a,m,_,_,_,_,_,tt,g,_ = self.ig.adv( f, a , m , 1 , gamma ) 
+                                
+                                #if (self.diff_g(g,gamma)): 
+                                    #ri =  self.model.Random.randint( ii+1 ) 
+                                    #f = F[:,ri]
+                                    #a = A[:,ri]
+                                    #m = M[:,ri]
+                                    #tt = 0
+                                    
+                            #F[:,ii] = f
+                            #A[:,ii] = a
+                            #M[:,ii] = m
+                            
+                            
+                
+                #if (myid==0):
+                    #FF = [F[:, np.arange(jj, self.walkers, self.comm.Get_size())]  for jj in np.arange(self.comm.Get_size() )  ] 
+                    #AA = [A[:, np.arange(jj, self.walkers, self.comm.Get_size())]  for jj in np.arange(self.comm.Get_size() )  ] 
+                    #MM = [M[:, np.arange(jj, self.walkers, self.comm.Get_size())]  for jj in np.arange(self.comm.Get_size() )  ] 
+                    #print "PARALLEL step, time: %f    events: %d." % (time, enum)
+                #else:
+                    #FF= None
+                    #AA = None
+                    #MM = None
+                    
+                #F = self.comm.scatter( FF  , root = 0 )
+                #A = self.comm.scatter( AA  , root = 0 )
+                #M = self.comm.scatter( MM  , root = 0 )
+                #time = self.comm.bcast( time , root=0 )
+                #gamma = self.comm.bcast( gamma , root=0 )
+
+                #print [myid , np.shape(F) ]
+
+                #ntasks = np.shape(F)[1]
+                
